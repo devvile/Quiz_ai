63f249d85e881b5e5763fbe64cbe87c2016c4e6e
devvile <patryk.czemierowski@gmail.com>
2025-08-11 16:47:35 +0200

    question template

--- src/pages/quiz/QuizPage.tsx
+++ src/pages/quiz/QuizPage.tsx
@@ -1,20 +1,21 @@
-import { Typography } from "@mui/material";
-import Question from "./components/Question";
+import { Typography, Card } from "@mui/material";
+import QuestionItem from "./components/Question";
 import { useQuestions } from "../../contexts/QuizContext";
 import { useEffect } from "react";
 
 const QuizPage = () => {
-  const {pickQuestionsForQuiz, questionsInQuiz } = useQuestions();
+  const { pickQuestionsForQuiz, questionsInQuiz, currentQuestion } =
+    useQuestions();
   useEffect(() => {
     pickQuestionsForQuiz(20);
   }, []);
-  console.log(questionsInQuiz)
+
+  console.log(questionsInQuiz);
 
   return (
-    <>
-      <Typography>QuizPage</Typography>
-      <Question />
-    </>
+    <Card sx={{p:4}}>
+      {questionsInQuiz.length>0 ? <QuestionItem question={questionsInQuiz[currentQuestion]}/> : null}
+    </Card>
   );
 };
 
--- src/pages/quiz/components/Question.tsx
+++ src/pages/quiz/components/Question.tsx
@@ -1,6 +1,35 @@
-const Question = ()=>{
-    return <>
-    </>
+import {
+  Button,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Divider,
+  Typography,
+} from "@mui/material";
+import type { Question } from "../../../types/Question";
+interface QuestionProps {
+  question: Question;
+  time?: number;
 }
+const QuestionItem = ({ question }: QuestionProps) => {
+  return (
+    <>
+      <CardHeader></CardHeader>
+      <CardContent>
+        <Typography sx={{ mb: 2 }} variant="h4">
+          {question.question}
+        </Typography>
+        <Divider></Divider>
+        {question.answers.map((answer, ind) => (
+          <p key={ind}>{answer}</p>
+        ))}
+      </CardContent>
+      <CardActions sx={{ position: "flex", justifyContent: "space-between" }}>
+        <div></div>
+        <Button variant="outlined">NEXT</Button>
+      </CardActions>
+    </>
+  );
+};
 
-export default Question;
+export default QuestionItem;
